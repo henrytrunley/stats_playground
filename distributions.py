0dfe376f6b7fb3ce482ef3f37b3f02aa25d20bd4
henrytrunley <ht369@alumni.cam.ac.uk>
2021-08-04 10:55:47 +0100

    create v1 of distributions.py with stable dists, and throwaway test script

---
+++ distributions.py
@@ -0,0 +1,87 @@
+import numpy as np
+from numba import jit
+
+class Stable:
+    sample_datapoints = 10**6
+
+    def __init__(self, alpha: float, beta: float, loc:float, scale:float):
+        self.alpha = alpha
+        self.beta = beta
+        self.scale = scale
+        self.loc = loc
+
+        self.x_width = 20 * scale
+
+    def sample(self, n: int):
+        return self.ppf(np.random.uniform(size=n, low=0., high=1.))
+
+    def cdf(self, x: np.array):
+        self._ensure_discrete_cdf()
+        return self._interpolate_discrete_cdf(x)
+
+    def pdf(self, x: np.array):
+        self._ensure_discrete_pdf()
+        return self._interpolate_discrete_pdf(x)
+
+    def ppf(self, x: np.array):
+        self._ensure_discrete_cdf()
+        return self._interpolate_discrete_ppf(x)
+
+    def _ensure_discrete_pdf(self):
+        if not hasattr(self, '_discrete_pdf'):
+            self._discrete_x_coords = np.linspace(self.loc - self.x_width, self.loc + self.x_width, self.sample_datapoints)
+            self._dx = self._discrete_x_coords[1] - self._discrete_x_coords[0]
+            n = len(self._discrete_x_coords)
+            f = np.fft.fftfreq(n, self._dx) * 2 * np.pi
+            cf = self.characteristic_function(f)
+            self._discrete_pdf = (np.fft.fftshift(np.fft.ifft(cf)) / self._dx).real
+
+    def _ensure_discrete_cdf(self):
+        self._ensure_discrete_pdf()
+        if not hasattr(self, '_discrete_cdf'):
+            self._discrete_cdf = np.cumsum(self._discrete_pdf * self._dx)
+
+    def _interpolate_discrete_cdf(self, x: np.array):
+        return np.interp(x, self._discrete_x_coords, self._discrete_cdf)
+
+    def _interpolate_discrete_pdf(self, x: np.array):
+        return np.interp(x, self._discrete_x_coords, self._discrete_pdf)
+
+    def _interpolate_discrete_ppf(self, x: np.array):
+        return np.interp(x, self._discrete_cdf, self._discrete_x_coords)
+
+    def characteristic_function(self, f: np.array):
+        return np.exp(
+            1j * f * self.loc - np.abs(self.scale * f) ** self.alpha * (1 - 1j * self.beta * np.sign(f) * self._phi(f))
+        )
+
+    def _phi(self, f):
+        if self.alpha == 1:
+            return - np.log(np.abs(self.scale * f)) * 2 / np.pi
+        else:
+            return np.tan(np.pi * self.alpha / 2)
+
+
+class Cauchy(Stable):
+    def __init__(self, x0: float, scale: float):
+        super().__init__(1., 0., x0, scale)
+
+    def pdf(self, x: np.array):
+        pass
+
+
+class Gaussian(Stable):
+    def __init__(self, mu: float, sigma: float):
+        super().__init__(2., 0., mu, sigma/np.sqrt(2))
+        self.sigma = sigma
+
+    def pdf(self, x: np.array):
+        return np.exp(- ((x - self.loc) / (self.sigma))**2 / 2) / (np.sqrt(2*np.pi) * self.sigma)
+
+
+class Levy(Stable):
+    def __init__(self, mu: float, c: float):
+        super().__init__(.5, 1., mu, c)
+
+    def pdf(self, x: np.array):
+        pass
---
+++ test.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+
+from distributions import *
+
+if __name__=="__main__":
+    stable = Stable(2, 0, 0, 1/np.sqrt(2))
+    # stable.sample_datapoints = 10**3
+    normal = Gaussian(0, 1)
+
+    plt.figure(figsize=(10, 10))
+    plt.hist(stable.sample(1000), bins=100)
+    plt.show()
+
+    # x = np.linspace(-10, 10, 1000)
+    # cdf = stable.cdf(x)
+    #
+    # plt.figure(figsize=(10, 10))
+    # plt.plot(x, cdf, label='stable CDF')
+    #
+    # plt.figure(figsize=(10, 10))
+    # x = np.linspace(.01, .99, 1000)
+    # ppf = stable.ppf(x)
+    # plt.plot(x, ppf, label='stable PPF')
+
+    # plt.figure(figsize=(10,10))
+    # pdf = stable.pdf(x)
+    # plt.plot(x, pdf, label='stable')
+    #
+    # pdf = normal.pdf(x)
+    # plt.plot(x, pdf, label='gaussian')
+    #
+
+    # plt.xlim(-10, 10)
+    # plt.legend()
+    # plt.show()
+
+    # df = f[1] - f[0]
+    # x = np.fft.fftfreq(len(f), d=df/(2*np.pi))
+    # y = np.fft.ifft(stable.characteristic_function(np.fft.fftshift(f)))
+    #
+    # plt.figure(figsize=(10, 10))
+    # plt.plot(x, y)
+    # plt.title('stable dist')
+    #
+    # plt.show()
+
